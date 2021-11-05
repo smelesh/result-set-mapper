b005898eb3f9cc9812743051f55627b3733ac9ac
Sergey Melesh <sergei.melesh@gmail.com>
2021-11-05 18:35:43 +0300

    Merge duplicate rows

---
+++ src/Embedded/MergeProcessor.php
@@ -0,0 +1,179 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Smelesh\ResultSetMapper\Embedded;
+
+/**
+ * Merges duplicate rows and embedded collections.
+ *
+ * When both items have the same distinct keys, the second item is merged into the first one
+ * according to the following rules:
+ * - non-array values are ignored;
+ * - embedded objects are merged recursively;
+ * - lists are appended (but not merged recursively!).
+ *
+ * When the path is specified, only items at this path of the current row will be affected.
+ *
+ * ATTENTION! Merge at the root path triggers FULL SCAN!
+ */
+final class MergeProcessor
+{
+    private string $path;
+    private array $distinctBy;
+
+    /**
+     * @param string $path Path in "dot" notation to a collection of items that should be merged.
+     *                     Empty string to merge root rows.
+     * @param list<string> $distinctBy List of fields to check in an item to distinct from other items.
+     *                                 Only scalar fields are allowed.
+     */
+    public function __construct(string $path, array $distinctBy)
+    {
+        if (empty($distinctBy)) {
+            throw new \InvalidArgumentException('Distinct key should be provided');
+        }
+
+        $this->path = $path;
+        $this->distinctBy = $distinctBy;
+    }
+
+    public function __invoke(\Traversable $rows): \Traversable
+    {
+        if ($this->path === '') {
+            yield from $this->mergeDuplicateRows($rows);
+        } else {
+            $pathSegments = explode('.', $this->path);
+
+            foreach ($rows as $row) {
+                $this->mapByPath(
+                    $row,
+                    $pathSegments,
+                    /** @param list<array<string, mixed>> $items */
+                    fn(array $items) => $this->mergeDuplicateRows($items)
+                );
+
+                yield $row;
+            }
+        }
+    }
+
+    /**
+     * @param array $data
+     * @param list<string> $path
+     * @param callable(array):array $mapper
+     */
+    private function mapByPath(array &$data, array $path, callable $mapper): void
+    {
+        $field = array_shift($path);
+
+        // ignore inaccessible path
+        if ($field === null || !isset($data[$field]) || !is_array($data[$field])) {
+            return;
+        }
+
+        $value = &$data[$field];
+
+        if ($path === []) {
+            // end of path reached, map value
+            if (array_is_list($value)) {
+                $value = $mapper($value);
+            }
+
+            return;
+        }
+
+        if (array_is_list($value)) {
+            // scan embedded collection
+            foreach ($value as &$item) {
+                $this->mapByPath($item, $path, $mapper);
+            }
+
+            unset($item);
+        } else {
+            // scan embedded object
+            $this->mapByPath($value, $path, $mapper);
+        }
+    }
+
+    /**
+     * @param iterable<array<string, mixed>> $rows
+     * @return list<array<string, mixed>>
+     */
+    private function mergeDuplicateRows(iterable $rows): array
+    {
+        $index = [];
+
+        foreach ($rows as $row) {
+            $distinctHash = $this->buildDistinctHash($row);
+
+            if (isset($index[$distinctHash])) {
+                $index[$distinctHash] = $this->mergeRowsRecursively($index[$distinctHash], $row);
+            } else {
+                $index[$distinctHash] = $row;
+            }
+        }
+
+        return array_values($index);
+    }
+
+    /**
+     * @param array<string, mixed> $originRow
+     * @param array<string, mixed> $duplicateRow
+     * @return array<string, mixed>
+     */
+    private function mergeRowsRecursively(array $originRow, array $duplicateRow): array
+    {
+        foreach ($duplicateRow as $key => $value) {
+            // ignore non-merging or missing fields
+            if (!is_array($value) || !isset($originRow[$key]) || !is_array($originRow[$key])) {
+                continue;
+            }
+
+            $isList = array_is_list($originRow[$key]);
+
+            if ($isList) {
+                if (!array_is_list($value)) {
+                    continue;
+                }
+
+                // append collection items
+                $originRow[$key] = array_merge($originRow[$key], $value);
+            } else {
+                if (array_is_list($value)) {
+                    continue;
+                }
+
+                // merge embedded objects
+                $originRow[$key] = $this->mergeRowsRecursively($originRow[$key], $value);
+            }
+        }
+
+        return $originRow;
+    }
+
+    /**
+     * @param array<string, mixed> $row
+     * @return string
+     */
+    private function buildDistinctHash(array $row): string
+    {
+        $result = [];
+
+        foreach ($this->distinctBy as $column) {
+            $value = $row[$column] ?? null;
+
+            if (!is_scalar($value)) {
+                throw new \LogicException(sprintf(
+                    'Only scalar values are allowed as distinct key, got "%s" at column "%s"',
+                    gettype($value),
+                    $column
+                ));
+            }
+
+            $result[$column] = $row[$column];
+        }
+
+        return implode("\0", $result);
+    }
+}
---
+++ tests/Embedded/MergeProcessorTest.php
@@ -0,0 +1,204 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Smelesh\ResultSetMapper\Tests\Embedded;
+
+use Smelesh\ResultSetMapper\Embedded\MergeProcessor;
+use PHPUnit\Framework\TestCase;
+use Smelesh\ResultSetMapper\ResultSet;
+
+class MergeProcessorTest extends TestCase
+{
+    public function testCreateWithoutDistinctKey(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Distinct key should be provided');
+
+        new MergeProcessor('', []);
+    }
+
+    public function testMergeWithSimpleDistinctKey(): void
+    {
+        $processor = new MergeProcessor('', ['id']);
+
+        $result = (new ResultSet([
+            ['id' => 101, 'type' => 'PREMIUM'],
+            ['id' => 102, 'type' => 'PREMIUM'],
+            ['id' => 101, 'type' => 'PREMIUM'], // duplicate
+        ]))->withProcessor($processor);
+
+        $this->assertSame([
+            ['id' => 101, 'type' => 'PREMIUM'],
+            ['id' => 102, 'type' => 'PREMIUM'],
+        ], $result->fetchAll());
+    }
+
+    public function testMergeWithCompositeDistinctKey(): void
+    {
+        $processor = new MergeProcessor('', ['id', 'app_id']);
+
+        $result = (new ResultSet([
+            ['id' => 101, 'app_id' => 'app1', 'type' => 'PREMIUM'],
+            ['id' => 102, 'app_id' => 'app1', 'type' => 'PREMIUM'],
+            ['id' => 101, 'app_id' => 'app1', 'type' => 'PREMIUM'], // duplicate
+            ['id' => 101, 'app_id' => 'app2', 'type' => 'PREMIUM'],
+        ]))->withProcessor($processor);
+
+        $this->assertSame([
+            ['id' => 101, 'app_id' => 'app1', 'type' => 'PREMIUM'],
+            ['id' => 102, 'app_id' => 'app1', 'type' => 'PREMIUM'],
+            ['id' => 101, 'app_id' => 'app2', 'type' => 'PREMIUM'],
+        ], $result->fetchAll());
+    }
+
+    public function testMergeWithMissingDistinctKey(): void
+    {
+        $this->expectException(\LogicException::class);
+        $this->expectExceptionMessage('Only scalar values are allowed as distinct key, got "NULL" at column "unknown"');
+
+        $processor = new MergeProcessor('', ['unknown']);
+
+        $result = (new ResultSet([
+            ['id' => 101, 'type' => 'PREMIUM'],
+        ]))->withProcessor($processor);
+
+        $result->fetchAll();
+    }
+
+    public function testMergeWithNonScalarDistinctKey(): void
+    {
+        $this->expectException(\LogicException::class);
+        $this->expectExceptionMessage('Only scalar values are allowed as distinct key, got "array" at column "options"');
+
+        $processor = new MergeProcessor('', ['options']);
+
+        $result = (new ResultSet([
+            ['id' => 101, 'options' => ['PREMIUM']],
+        ]))->withProcessor($processor);
+
+        $result->fetchAll();
+    }
+
+    public function testMergeWithEmbeddedCollection(): void
+    {
+        $processor = new MergeProcessor('', ['id']);
+
+        $result = (new ResultSet([
+            ['id' => 101, 'type' => 'PREMIUM', 'features' => [
+                ['id' => 'SUBTITLES', 'name' => 'Show subtitles'],
+                ['id' => 'UPLOAD', 'name' => 'Allow uploads'],
+            ]],
+            ['id' => 101, 'type' => 'PREMIUM', 'features' => [ // duplicate
+                ['id' => 'SUBTITLES', 'name' => 'Show subtitles'],
+                ['id' => 'ADD_COMMENT', 'name' => 'Add comments'],
+            ]],
+            ['id' => 102, 'type' => 'PREMIUM', 'features' => [
+                ['id' => 'SUBTITLES', 'name' => 'Show subtitles'],
+            ]],
+        ]))->withProcessor($processor);
+
+        $this->assertSame([
+            ['id' => 101, 'type' => 'PREMIUM', 'features' => [
+                ['id' => 'SUBTITLES', 'name' => 'Show subtitles'],
+                ['id' => 'UPLOAD', 'name' => 'Allow uploads'],
+                ['id' => 'SUBTITLES', 'name' => 'Show subtitles'],
+                ['id' => 'ADD_COMMENT', 'name' => 'Add comments'],
+            ]],
+            ['id' => 102, 'type' => 'PREMIUM', 'features' => [
+                ['id' => 'SUBTITLES', 'name' => 'Show subtitles'],
+            ]],
+        ], $result->fetchAll());
+    }
+
+    public function testMergeWithEmbeddedItem(): void
+    {
+        $processor = new MergeProcessor('', ['id']);
+
+        $result = (new ResultSet([
+            ['id' => 101, 'type' => 'PREMIUM', 'product' => [
+                'id' => 1001, 'name' => 'Premium Monthly', 'options' => ['SUBTITLES', 'UPLOAD'],
+            ]],
+            ['id' => 101, 'type' => 'PREMIUM', 'product' => [ // duplicate
+                'id' => 1001, 'name' => 'Premium Monthly', 'options' => ['SUBTITLES', 'ADD_COMMENT'],
+            ]],
+            ['id' => 102, 'type' => 'PREMIUM', 'product' => [
+                'id' => 1001, 'name' => 'Premium Monthly', 'options' => ['SUBTITLES'],
+            ]],
+        ]))->withProcessor($processor);
+
+        $this->assertSame([
+            ['id' => 101, 'type' => 'PREMIUM', 'product' => [
+                'id' => 1001, 'name' => 'Premium Monthly', 'options' => ['SUBTITLES', 'UPLOAD', 'SUBTITLES', 'ADD_COMMENT'],
+            ]],
+            ['id' => 102, 'type' => 'PREMIUM', 'product' => [
+                'id' => 1001, 'name' => 'Premium Monthly', 'options' => ['SUBTITLES'],
+            ]],
+        ], $result->fetchAll());
+    }
+
+    public function testMergeAtSpecificPath(): void
+    {
+        $processor = new MergeProcessor('subscriptions.features', ['id']);
+
+        $result = (new ResultSet([
+            ['id' => 1, 'name' => 'user #1', 'subscriptions' => [
+                ['id' => 101, 'type' => 'PREMIUM', 'features' => [
+                    ['id' => 'SUBTITLES', 'name' => 'Show subtitles'],
+                    ['id' => 'UPLOAD', 'name' => 'Allow uploads'],
+                    ['id' => 'SUBTITLES', 'name' => 'Show subtitles'], // duplicate
+                ]],
+                ['id' => 101, 'type' => 'PREMIUM', 'features' => [
+                    ['id' => 'UPLOAD', 'name' => 'Allow uploads'],
+                    ['id' => 'UPLOAD', 'name' => 'Allow uploads'], // duplicate
+                ]],
+                ['id' => 102, 'type' => 'PREMIUM', 'features' => [
+                    ['id' => 'SUBTITLES', 'name' => 'Show subtitles'],
+                ]],
+            ]],
+            ['id' => 2, 'name' => 'user #2', 'subscriptions' => [
+                ['id' => 101, 'type' => 'PREMIUM', 'features' => [
+                    ['id' => 'SUBTITLES', 'name' => 'Show subtitles'],
+                    ['id' => 'ADD_COMMENT', 'name' => 'Add comments'],
+                    ['id' => 'ADD_COMMENT', 'name' => 'Add comments'], // duplicate
+                ]],
+            ]],
+        ]))->withProcessor($processor);
+
+        $this->assertSame([
+            ['id' => 1, 'name' => 'user #1', 'subscriptions' => [
+                ['id' => 101, 'type' => 'PREMIUM', 'features' => [
+                    ['id' => 'SUBTITLES', 'name' => 'Show subtitles'],
+                    ['id' => 'UPLOAD', 'name' => 'Allow uploads'],
+                ]],
+                ['id' => 101, 'type' => 'PREMIUM', 'features' => [
+                    ['id' => 'UPLOAD', 'name' => 'Allow uploads'],
+                ]],
+                ['id' => 102, 'type' => 'PREMIUM', 'features' => [
+                    ['id' => 'SUBTITLES', 'name' => 'Show subtitles'],
+                ]],
+            ]],
+            ['id' => 2, 'name' => 'user #2', 'subscriptions' => [
+                ['id' => 101, 'type' => 'PREMIUM', 'features' => [
+                    ['id' => 'SUBTITLES', 'name' => 'Show subtitles'],
+                    ['id' => 'ADD_COMMENT', 'name' => 'Add comments'],
+                ]],
+            ]],
+        ], $result->fetchAll());
+    }
+
+    public function testMergeAtMissingPath(): void
+    {
+        $processor = new MergeProcessor('subscription.features', ['id']);
+
+        $result = (new ResultSet([
+            ['id' => 1, 'name' => 'user #1', 'subscription' => null],
+            ['id' => 2, 'name' => 'user #2', 'subscription' => ['id' => 101, 'type' => 'PREMIUM', 'features' => []]],
+        ]))->withProcessor($processor);
+
+        $this->assertSame([
+            ['id' => 1, 'name' => 'user #1', 'subscription' => null],
+            ['id' => 2, 'name' => 'user #2', 'subscription' => ['id' => 101, 'type' => 'PREMIUM', 'features' => []]],
+        ], $result->fetchAll());
+    }
+}
