0c47e78a3b1906d7cc042c2de358b22f94c63a50
Sergey Melesh <sergei.melesh@gmail.com>
2021-11-09 22:43:04 +0300

    Support json and datetime types

--- src/Type/SimpleTypeConverter.php
+++ src/Type/SimpleTypeConverter.php
@@ -12,12 +12,26 @@ final class SimpleTypeConverter implements TypeConverter
             return null;
         }
 
-        return match ($type) {
-            'int' => (int) $value,
-            'float' => (float) $value,
-            'bool' => (bool) $value,
-            'string' => (string) $value,
-            default => throw new \InvalidArgumentException(sprintf('Unknown type "%s"', $type)),
-        };
+        try {
+            $result = match ($type) {
+                'int' => (int)$value,
+                'float' => (float)$value,
+                'bool' => (bool)$value,
+                'string' => (string)$value,
+                'datetime' => new \DateTimeImmutable($value),
+                'json' => json_decode($value, true, flags: JSON_THROW_ON_ERROR),
+                default => throw new \InvalidArgumentException(sprintf('Unknown type "%s"', $type)),
+            };
+        } catch (\InvalidArgumentException $e) {
+            throw $e;
+        } catch (\Throwable $e) {
+            throw new \UnexpectedValueException(sprintf(
+                'Unable to convert value of type "%s" to type "%s"',
+                gettype($value),
+                $type,
+            ), 0, $e);
+        }
+
+        return $result;
     }
 }
--- tests/Type/SimpleTypeConverterTest.php
+++ tests/Type/SimpleTypeConverterTest.php
@@ -10,9 +10,9 @@ use PHPUnit\Framework\TestCase;
 class SimpleTypeConverterTest extends TestCase
 {
     /**
-     * @dataProvider provideSupportedTypes
+     * @dataProvider provideSupportedSimpleTypes
      */
-    public function testConvertSupportedType(string $type, mixed $value, mixed $expectedResult): void
+    public function testConvertSupportedSimpleType(string $type, mixed $value, mixed $expectedResult): void
     {
         $converter = new SimpleTypeConverter();
 
@@ -21,7 +21,7 @@ class SimpleTypeConverterTest extends TestCase
         $this->assertSame($expectedResult, $result);
     }
 
-    public function provideSupportedTypes(): iterable
+    public function provideSupportedSimpleTypes(): iterable
     {
         yield ["int", "10.99", 10];
         yield ["int", 10.99, 10];
@@ -51,5 +51,70 @@ class SimpleTypeConverterTest extends TestCase
         $converter = new SimpleTypeConverter();
 
         $converter->convert('test', 'unknown');
+    }
+
+    /**
+     * @dataProvider provideValidDateTime
+     */
+    public function testConvertValidDateTime(mixed $value, ?\DateTimeImmutable $expectedResult): void
+    {
+        $converter = new SimpleTypeConverter();
+
+        $result = $converter->convert($value, 'datetime');
+
+        $this->assertEquals($expectedResult, $result);
+    }
+
+    public function provideValidDateTime(): iterable
+    {
+        yield [null, null];
+        yield ['2021-11-09 21:55:59', new \DateTimeImmutable('2021-11-09 21:55:59.000000 +00:00')];
+        yield ['2021-11-09 21:55:59.123456', new \DateTimeImmutable('2021-11-09 21:55:59.123456 +00:00')];
+        yield ['2021-11-09 21:55:59.123', new \DateTimeImmutable('2021-11-09 21:55:59.123000 +00:00')];
+        yield ['2021-11-09 21:55:59+03', new \DateTimeImmutable('2021-11-09 21:55:59.000000 +03:00')];
+        yield ['2021-11-09 21:55:59.123+03', new \DateTimeImmutable('2021-11-09 21:55:59.123000 +03:00')];
+    }
+
+    public function testConvertInvalidDateTime(): void
+    {
+        $this->expectException(\UnexpectedValueException::class);
+        $this->expectExceptionMessage('Unable to convert value of type "string" to type "datetime"');
+
+        $converter = new SimpleTypeConverter();
+
+        $converter->convert('09/Nov/2011 21:55:59', 'datetime');
+    }
+
+    /**
+     * @dataProvider provideValidJsonString
+     */
+    public function testConvertValidJson(mixed $value, mixed $expectedResult): void
+    {
+        $converter = new SimpleTypeConverter();
+
+        $result = $converter->convert($value, 'json');
+
+        $this->assertSame($expectedResult, $result);
+    }
+
+    public function provideValidJsonString(): iterable
+    {
+        yield [null, null];
+        yield ['{"id": 10, "type": "PREMIUM"}', ['id' => 10, 'type' => 'PREMIUM']];
+        yield ['[10, 20, 30]', [10, 20, 30]];
+        yield ['null', null];
+        yield ['"string"', 'string'];
+        yield ['{}', []];
+        yield ['[]', []];
+    }
+
+    public function testConvertInvalidJson(): void
+    {
+        $this->expectException(\UnexpectedValueException::class);
+        $this->expectExceptionMessage('Unable to convert value of type "string" to type "json"');
+
+        $converter = new SimpleTypeConverter();
+
+        $converter->convert('invalid_json', 'json');
     }
 }
